b53023cbdd80ed3ccb6318b826b6a9ebed20cbcc
goodliu <zenghao235711@gmail.com>
2023-10-16 21:20:04 +0800

    all: update copyright headers
    to keep consistent with the description in the CONTRIBUTING.md file of the trpc repository
    
    https://github.com/trpc-group/trpc/blob/main/CONTRIBUTING.md#copyright-headers

--- runtime_unix.go
+++ runtime_unix.go
@@ -1,8 +1,15 @@
+//
+//
 // Tencent is pleased to support the open source community by making tRPC available.
-// Copyright (C) 2023 THL A29 Limited, a Tencent company. All rights reserved.
+//
+// Copyright (C) 2023 THL A29 Limited, a Tencent company.
+// All rights reserved.
+//
 // If you have downloaded a copy of the tRPC source code from Tencent,
 // please note that tRPC source code is licensed under the Apache 2.0 License,
 // A copy of the Apache 2.0 License is included in this file.
+//
+//
 
 // go:build !windows
 
@@ -74,7 +81,7 @@ func getPidCount(ctx context.Context) {
 
 // getTcpSocket get metrics of TCP
 func getTcpSocket() {
-	/// proc/net/sockstat
+	// / proc/net/sockstat
 	st, err := os.Open("/proc/net/sockstat")
 	if err != nil {
 		return
